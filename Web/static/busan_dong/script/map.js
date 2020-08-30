8ad642f1b8a8146896d602cd698fea54fa007022
kho903 <gmldnr2222@naver.com>
2020-08-30 21:02:17 +0900

    부산 각 동별 통계 항목별 지도 시각화 완료

--- Web/static/busan_dong/script/map.js
+++ Web/static/busan_dong/script/map.js
@@ -1,4 +1,4 @@
-function busan_dong_map(_mapContainerId, _spots, dict_high) {
+function busan_dong_map(_mapContainerId, _spots, dict_high, dict_pump, dict_manhole, dict_imp) {
     var WIDTH, HEIGHT,
         CENTERED,
         MAP_CONTAINER_ID = _mapContainerId,
@@ -49,11 +49,11 @@ function busan_dong_map(_mapContainerId, _spots, dict_high) {
                 .data(features)
                 .enter().append("path")
 
-                .attr("style", function(d,i){
-                    var each_level = dict_high[d.properties.EMD_KOR_NM];
-                    console.log(Math.ceil(each_level));
-                    return "fill: " + color(Math.ceil(each_level));
-                })
+                // .attr("style", function(d,i){
+                //     var each_level = dict_high[d.properties.EMD_KOR_NM];
+                //     console.log(Math.ceil(each_level));
+                //     return "fill: " + color(Math.ceil(each_level));
+                // })
 
                 .attr("class", function (d) {
                     return "municipality c " + d.properties.EMD_KOR_NM;
@@ -69,9 +69,9 @@ function busan_dong_map(_mapContainerId, _spots, dict_high) {
                 })
                 .attr("dy", ".35em")
                 .attr("class", "municipality-label")
-                // .text(function (d) {
-                //     return d.properties.EMD_KOR_NM;
-                // });
+            // .text(function (d) {
+            //     return d.properties.EMD_KOR_NM;
+            // });
 
             callback();
         });
@@ -82,22 +82,73 @@ function busan_dong_map(_mapContainerId, _spots, dict_high) {
             .data(_spots).enter()
             .append("circle")
             .attr("class", "spot")
-            .attr("cx", function (d) {
-                return projection([d.lon, d.lat])[0];
+            .attr("cx", function (d, i) {
+                console.log("프로젝션 ", projection([d.lon, d.lat])[0]);
+                return [100, 130, 160, 190][i];
             })
             .attr("cy", function (d) {
-                return projection([d.lon, d.lat])[1];
+                return [230];
             })
-            .attr("r", "2px")
-            .attr("fill", "red")
+            .attr("r", "10px")
+            .attr("fill", function (d, i) {
+                return ["brown", "rgb(0, 99, 132)", "rgb(77, 11, 88)", "rgb(255, 99, 132)"][i]
+            })
             .on('click', spot_clicked_event)
             .transition()
             .ease(d3.easeElastic);
     }
 
-    function spot_clicked_event(d) {
-        alert(d['tag']);
+    function spot_clicked_event(d, p) {
+        console.log(p);
+        var color;
+        var each_level;
+        switch (p) {
+            case 0:
+                color = d3.scaleLinear()
+                    .domain([0, 100])
+                    .range(["#f2dfd3", "#964b00"]);
+                break;
+            case 1:
+                color = d3.scaleLinear()
+                    .domain([0, 100])
+                    .range(["rgb(184, 237, 255)", "rgb(0, 99, 132)"]);
+                break;
+            case 2:
+                color = d3.scaleLinear()
+                    .domain([0, 100])
+                    .range(["rgb(250, 219, 255)", "rgb(77, 11, 88)"]);
+                break;
+            case 3:
+                color = d3.scaleLinear()
+                    .domain([0, 100])
+                    .range(["rgb(255, 240, 243)", "rgb(255, 99, 132)"]);
+                break;
+        }
+
+        map.selectAll("path")
+            .data(features)
+            // .enter().append("path")
+
+            .attr("style", function (d, i) {
+                console.log(p);
+                switch (p) {
+                    case 0:
+                        each_level = dict_high[d.properties.EMD_KOR_NM]*2;
+                        break;
+                    case 1:
+                        each_level = dict_pump[d.properties.EMD_KOR_NM] * 500000000;
+                        break;
+                    case 2:
+                        each_level = dict_manhole[d.properties.EMD_KOR_NM]*5000;
+                        break;
+                    case 3:
+                        each_level = dict_imp[d.properties.EMD_KOR_NM]*2.5;
+                        break;
+                }
+                return "fill: " + color(Math.ceil(each_level));
+            })
     }
+
 
     function province_clicked_event(d) {
         var x, y, zoomLevel;
@@ -106,10 +157,7 @@ function busan_dong_map(_mapContainerId, _spots, dict_high) {
             var centroid = path.centroid(d);
             x = centroid[0];
             y = centroid[1];
-            if (d.properties.EMD_KOR_NM == '강서구' || d.properties.EMD_KOR_NM == '기장군')
-                zoomLevel = 1.5;
-            else
-                zoomLevel = 2;
+            zoomLevel = 2;
             CENTERED = d;
             // console.log('centered', CENTERED);
         } else {
